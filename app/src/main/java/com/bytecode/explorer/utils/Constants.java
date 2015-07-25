2c2c88deaa568c2416fd3b380ce84b8c6f05c122
Rakshith B P <Pritam.rakshi@gmail.com>
2015-07-25 12:40:06 +0530

    rest client

---
+++ app/src/main/java/com/bytecode/explorer/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.bytecode.explorer.utils;
+
+/**
+ * Created by Rakshith on 25-07-2015.
+ */
+public class Constants {
+
+    public static final String BASE_URL =  "http://10.0.1.50:8080/explorer-server/";
+    public static final String NEARBY_TRIPS = "SearchQuery";
+
+}
